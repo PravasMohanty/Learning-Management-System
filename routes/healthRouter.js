32e592c1d4a1cbfe965c3841970abce02272768c
Pravas Mohanty <pravasmohanty1000@gmail.com>
2026-06-01 02:08:55 +0530

    Setting up FOlder Structure

--- routes/healthRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const healthRouter = express.Router()
-
-healthRouter.get('/', (req, res) => {
-    res.json({ message: 'Server is running' })
-})
-
-module.exports = healthRouter
